1f256576acac9e21782378118de2cfe3b560fc13
Quique <60704779+Quique20@users.noreply.github.com>
2022-02-07 20:33:21 +0100

    Update hit_counter.php

--- hit_counter.php
+++ hit_counter.php
@@ -3,42 +3,47 @@
         $username = 'alumno';
         $password = 'contraseña';
         try{
-            $conn = new PDO ("mysql:host=$servername;dbname=visitantes", $username, $password);
+            $conn = new PDO ("mysql:host=127.0.0.1;dbname=visitantes", $username, $password);
             $siteVisitsMap  = 'siteStats';
             $visitorHashKey = '';
-                if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+               if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+
                         $visitorHashKey = $_SERVER['HTTP_CLIENT_IP'];
+
                  } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+
                         $visitorHashKey = $_SERVER['HTTP_X_FORWARDED_FOR'];
+
                 } else {
+
                         $visitorHashKey = $_SERVER['REMOTE_ADDR'];
                 }
+
                 $totalVisits = 0 ;
 
-                $sql = "SELECT ip, contador FROM visitas WHERE ip = :ip";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(":ip", $visitorHashKey);
-
-                $stmt->setFetchMode(PDO::FETCH_ASSOC);
-                $result = $stmt->execute();
+                $query = $conn->prepare("SELECT ip, contador FROM visitas WHERE ip = :ip");
+                $query = bindParam(":ip",$visitorHashKey);
+                $query-> execute();
+                $result = $query -> fetch(PDO::FETCH_ASSOC);
 
                 if ($result != TRUE){
                     $totalVisits = 1;
-                    $sql = "INSERT INTO visitas (ip,contador) VALUES :ip, :contador ON DUPLICATE KEY UPDATE contador = :contador ; ";
+                    $sql = "INSERT INTO visitas (ip,contador) VALUES (:ip, :contador)";
                     $stmt = $conn->prepare($sql);
-                    $stmt->bindParam('ip', $visitorHashKey);
-                    $stmt->bindParam('contador',$totalVisits);
+                    $stmt->bindParam(':ip', $visitorHashKey);
+                    $stmt->bindParam(':contador',$totalVisits);
                     $stmt->execute();
+                    echo "Bienvenido, has entrado en la página " .$totalVisits.".";
                 }else{
-                    $totalVisits = $row['contador'] + 1;
-                    $sql = "UPDATE visitas SET contador=contador WHERE ip=:ip";
+                    $totalVisits = $result['contador']+1;
+                    $sql = "UPDATE visitas SET contador = :contador WHERE ip = :ip";
                     $stmt = $conn->prepare($sql);
-                    $stmt->bindParam('ip', $visitorHashKey);
-                    $stmt->bindParam('contador',$totalVisits);
+                    $stmt->bindParam(':ip', $visitorHashKey);
+                    $stmt->bindParam(':contador',$totalVisits);
                     $stmt->execute();
+                    echo "Bienvenido, has entrado en la página " .$totalVisits.".";
                 }
-                echo "Bienvenido, has entrado en la página " .$totalVisits.".";
         }catch (Exception e){
-            
-        }
+
+         }
 ?>
