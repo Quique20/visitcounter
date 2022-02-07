455877e54066570b2c7a3d7e7b466305277944cd
Quique <60704779+Quique20@users.noreply.github.com>
2022-02-07 20:46:44 +0100

    Update hit_counter.php

--- hit_counter.php
+++ hit_counter.php
@@ -1,49 +1,49 @@
 <?php
-        $servername = '127.0.0.1';
-        $username = 'alumno';
-        $password = 'contraseña';
-        try{
-            $conn = new PDO ("mysql:host=127.0.0.1;dbname=visitantes", $username, $password);
-            $siteVisitsMap  = 'siteStats';
-            $visitorHashKey = '';
-               if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-
-                        $visitorHashKey = $_SERVER['HTTP_CLIENT_IP'];
-
-                 } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-
-                        $visitorHashKey = $_SERVER['HTTP_X_FORWARDED_FOR'];
-
-                } else {
-
-                        $visitorHashKey = $_SERVER['REMOTE_ADDR'];
-                }
-
-                $totalVisits = 0 ;
-
-                $query = $conn->prepare("SELECT ip, contador FROM visitas WHERE ip = :ip");
-                $query = bindParam(":ip",$visitorHashKey);
-                $query-> execute();
-                $result = $query -> fetch(PDO::FETCH_ASSOC);
-
-                if ($result == TRUE){
-                    $totalVisits = 1;
-                    $sql = "INSERT INTO visitas (ip,contador) VALUES (:ip, :contador)";
-                    $stmt = $conn->prepare($sql);
-                    $stmt->bindParam(':ip', $visitorHashKey);
-                    $stmt->bindParam(':contador',$totalVisits);
-                    $stmt->execute();
-                    echo "Bienvenido, has entrado en la página " .$totalVisits.".";
-                }else{
-                    $totalVisits = $result['contador']+1;
-                    $sql = "UPDATE visitas SET contador = :contador WHERE ip = :ip";
-                    $stmt = $conn->prepare($sql);
-                    $stmt->bindParam(':ip', $visitorHashKey);
-                    $stmt->bindParam(':contador',$totalVisits);
-                    $stmt->execute();
-                    echo "Bienvenido, has entrado en la página " .$totalVisits.".";
-                }
-        }catch (Exception e){
-
-         }
-?>
+ 2         $servername = '127.0.0.1';
+ 3         $username = 'alumno';
+ 4         $password = 'contraseña';
+ 5         try{
+ 6             $conn = new PDO ("mysql:host=127.0.0.1;dbname=visitantes", $username, $password);
+ 7             $siteVisitsMap  = 'siteStats';
+ 8             $visitorHashKey = '';
+ 9                if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+10
+11                         $visitorHashKey = $_SERVER['HTTP_CLIENT_IP'];
+12
+13                  } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+14
+15                         $visitorHashKey = $_SERVER['HTTP_X_FORWARDED_FOR'];
+16
+17                 } else {
+18
+19                         $visitorHashKey = $_SERVER['REMOTE_ADDR'];
+20                 }
+21
+22                 $totalVisits = 0 ;
+23
+24                 $query = $conn->prepare("SELECT ip, contador FROM visitas WHERE ip = :ip");
+25                 $query -> bindParam(":ip",$visitorHashKey);
+26                 $query-> execute();
+27                 $result = $query -> fetch(PDO::FETCH_ASSOC);
+28
+29                 if ($result == FALSE){
+30                     $totalVisits = 1;
+31                     $sql = "INSERT INTO visitas (ip,contador) VALUES (:ip, :contador)";
+32                     $stmt = $conn->prepare($sql);
+33                     $stmt->bindParam(':ip', $visitorHashKey);
+34                     $stmt->bindParam(':contador',$totalVisits);
+35                     $stmt->execute();
+36                     echo "Bienvenido, has entrado en la página " .$totalVisits.".";
+37                 }else{
+38                     $totalVisits = $result['contador']+1;
+39                     $sql = "UPDATE visitas SET contador = :contador WHERE ip = :ip";
+40                     $stmt = $conn->prepare($sql);
+41                     $stmt->bindParam(':ip', $visitorHashKey);
+42                     $stmt->bindParam(':contador',$totalVisits);
+43                     $stmt->execute();
+44                     echo "Bienvenido, has entrado en la página " .$totalVisits.".";
+45                 }
+46         }catch (Exception $e){
+47
+48          }
+49 ?>
